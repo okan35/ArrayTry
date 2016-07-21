23009e327f9ea9ec0a180c0f32b7d12a41c33410
cimler <okankaya_35@windowslive.com>
2016-07-21 17:27:50 +0200

    Reverse sorted array in C#
    After coding for some time on Java, I wanted to check if I can apply the
    things I know on Java to C# and I was succesful.

---
+++ ArrayTry/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            List<String> words = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Type a word: ");
+                string answer = Console.ReadLine();
+                words.Add(answer);
+                if (answer.Equals(""))
+                {
+                    words.Reverse();
+                    Console.WriteLine("You typed the following words:  ");
+                    foreach (string word in words)
+                    {
+                        Console.WriteLine(word);
+                        
+                    }
+                    break;
+                }
+
+
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
